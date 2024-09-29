dc67eb9fd88f08c8d1a317d9b2c78d0b2700ffde
solomontudu <tukintudu@gmail.com>
2024-09-29 20:55:57 +0530

    added the calculation functionality, to calculate and outout the final result in the display, also added the functionality to continue to keep calculation with the result

--- script.js
+++ script.js
@@ -29,20 +29,34 @@ function addDecimal() {
   }
 }
 
+// calculate first and second values depending of operator
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
+
 // operator function
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
+  // prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
   // if there is no previous value, store current value as first value
   if (!firstValue) {
     firstValue = currentValue;
   } else {
-    console.log("crrent", currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
   }
   // ready for the next value, store operator
   awaitingNextValue = true;
   operatorValue = operator;
-  console.log("firstValue", firstValue);
-  console.log("operator", operator);
 }
 
 // add event listener for numbers, operators and decimal buttons
